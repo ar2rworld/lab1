e01c8500d71de3de59e6bbbb4a6e0c571412f388
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2021-02-10 15:27:37 -0800

    test qrcode page 0

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
+<?php
+$text = basename($_SERVER['PHP_SELF']); 
+$path = './'; 
+$file = $path.uniqid().".png"; 
+// $ecc stores error correction capability('L') 
+$ecc = 'L'; 
+$pixel_Size = 10; 
+$frame_Size = 10; 
+// Generates QR Code and Stores it in directory given 
+QRcode::png($text, $file, $ecc, $pixel_Size, $frame_size); 
+  
+// Displaying the stored QR code from directory 
+echo "<center><img src='".$file."'></center>"; 
+?>
 <html>
 <body>
 <h1 style="text-align: center;">Hello World, need to add some more style</h1>
+<h1 style="text-align: center;">Mural example:<img src='https://www.tourismvernon.com/en/resourcesGeneral/Images/caetani-mural.jpg' alt='mural image'></img></h1>
 </body>
 </html>
