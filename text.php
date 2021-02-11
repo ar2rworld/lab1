93112f2c8d19488665f6d765d07e5374927a6141
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2021-02-10 17:54:15 -0800

    test qrcode page 9

--- text.php
+++ text.php
@@ -1,33 +1,30 @@
 <?php
-include 'phpqrcode/qrlib.php'; 
+include '/usr/share/phpqrcode/qrlib.php'; 
 $text = 'https://tristendvernychuk.ca/dina/' . basename($_SERVER['PHP_SELF']);//index.php 
 echo $text;
 
 
-//$path = './'; 
-//$file = $path.uniqid().".png";
-//echo $file;
-////$ecc stores error correction capability('L') 
-//$ecc = 'L'; 
-//$pixel_Size = 10; 
-//$frame_Size = 10; 
-//// Generates QR Code and Stores it in directory given 
-//QRcode::png($text, $file, $ecc, $pixel_Size, $frame_size); 
+$path = './'; 
+$file = $path.uniqid().".png";
+echo $file;
+//$ecc stores error correction capability('L') 
+$ecc = 'L'; 
+$pixel_Size = 10; 
+/$frame_Size = 10; 
+// Generates QR Code and Stores it in directory given 
+QRcode::png($text, $file, $ecc, $pixel_Size, $frame_size); 
   
-//// Displaying the stored QR code from directory 
-//echo "<center><img src='".$file."'></center>"; 
+// Displaying the stored QR code from directory 
+echo "<center><img src='".$file."'></center>"; 
 // generating
-    $textOut = QRcode::text($codeContents);
-    $raw = join("<br/>", $textOut);
-    
-    $raw = strtr($raw, array(
-        '0' => '<span style="color:white">&#9608;&#9608;</span>',
-        '1' => '&#9608;&#9608;'
-    ));
-    
-    // displaying
-    
-    echo '<tt style="font-size:7px">'.$raw.'</tt>'; 
+//$textOut = QRcode::text($codeContents);
+//$raw = join("<br/>", $textOut);
+//$raw = strtr($raw, array(
+//    '0' => '<span style="color:white">&#9608;&#9608;</span>',
+//    '1' => '&#9608;&#9608;'
+//));
+// displaying
+//echo '<tt style="font-size:7px">'.$raw.'</tt>'; 
 ?>
 <html>
 <body>
