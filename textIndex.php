d57fa0e8753e708ea1b6d6f4a21d121be576173c
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2021-02-10 18:32:46 -0800

    test qrcode page 12

--- textIndex.php
+++ textIndex.php
@@ -2,6 +2,6 @@
 <body>
 <h1 style="text-align: center;">Hello World, need to add some more style</h1>
 <h1 style="text-align: center;">Mural example:<img src='https://www.tourismvernon.com/en/resourcesGeneral/Images/caetani-mural.jpg' alt='mural image'></img></h1>
-<img src='https://tristendvernychuk.ca/dina/text2.php?url=https://tristendvernychuk.ca/dina/textIndex.php'></img>
+<img src='https://tristendvernychuk.ca/dina/text2.php?u=https://tristendvernychuk.ca/dina/textIndex.php'></img>
 </body>
 </html>
