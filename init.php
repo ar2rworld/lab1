77a5ff513d8f9ef826596e034ea1e72bc27e9005
ragingbonjour <tristen.dvernychuk@myokanagan.bc.ca>
2021-03-05 23:53:15 +0000

    Hashing support in QR generation - no repeat generations of the same URL

--- init.php
+++ init.php
@@ -1,15 +1,20 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Allow error messages to show by default - here for easier toggling
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 
-include('/usr/share/phpqrcode/qrlib.php');
+// Include scripts manually since apache pretends it doesn't exist otherwise
+	require('/usr/share/phpqrcode/qrlib.php');
+// TEXT / SVG Generation Libraries
+	require('/usr/share/phpqrcode/qrmask.php');
+	require('/usr/share/phpqrcode/qrspec.php');
 
-// Removes caching on QR codes
-header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', FALSE);
-header('Pragma: no-cache');
+// General headers sent with responses, CloudFlare mostly takes care of them
+	header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+	header('Cache-Control: post-check=0, pre-check=0', FALSE);
+	header('Pragma: no-cache');
 
 ?>
---
+++ qr.php
@@ -0,0 +1,54 @@
+<?php
+
+include('init.php');				// Include headers & basic library imports
+$txt         = $_GET['url']  ?? 'www.downtownvernon.com';
+$pixel_size  = $_GET['ps']   ??  15;		// General QR-Code size
+$frame_size  = $_GET['fs']   ??  2;		// Buffer around the image of whitespace
+$format      = $_GET['fo']   ?? 'jpg';		/* Can be jpg, png, svg according to the library
+						   Some logic will be required to make this work however...
+						   Especially SVG which is not a format, but a string
+                                        	   Jpg was choosen for how small it is (<= 500 bytes each) and for compatibility */
+$imageWidth  = $_GET['iw']   ??  250;		// SVG only, in PX
+$saveToFile  = $_GET['save'] ??  true;		// SVG only, whether to output stream or save to the filename function
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+if (in_array($format, array("jpg", "jpeg", "png", "svg", "txt"))) {
+	$file = "qr_".md5($url.$pixel_size.$frame_size).".".$format;	// Assign unique ID filename IF nothing is specified
+} else {
+	$file = "qr_".md5($url.$pixel_size.$frame_size).".jpg";		// Save as JPEG if the format is malformed
+}									// In SVG if a filename is set saveToFile would be ignored
+
+$ecc         = 'QR_ECLEVEL_L';			// QR code Error Correction strength. Higher = more resilient but larger
+$tempDir     = "./images/";			// Image storage location
+
+if (scandir($folder).in_array($file)) {		// Check file hash against already generated images
+	header('Location: '.$tempDir.$file);
+} else {
+	switch($format) {
+		case "png":
+			QRcode::png($txt, $tempDir.$file, $ecc, $pixel_size, $frame_size);
+        		header('Location: '.$tempDir.$file);
+        		break;
+		case "svg":			// Takes funkier parameters to get this working...alot of ways it can go wrong
+			$svgCode = QRcode::svg($svg, $tempDir.$file, $saveToFile, $ecc, $imageWidth);
+			echo $svgCode;
+			break;
+		case "jpg":
+		case "jpeg":			// Using the PNG function to save as JPG works I guess? A bit redundant
+			QRcode::png($txt, $tempDir.$file, $ecc, $pixel_size, $frame_size);
+        		header('Location: '.$tempDir.$file);
+	        	break;
+		case "text":
+			QRcode::text($txt);	// No parameters required for raw output
+	    		//echo '<pre>';		// Display the contents just in the page, can't really save to file anyways
+	    		//echo join("\n", $txt);
+    			//echo '</pre>';
+			break;
+		default:
+			QRcode::png($txt, $tempDir.$file, $ecc, $pixel_size, $frame_size);
+                        header('Location: '.$tempDir.$file);
+                        break;
+	}
+}
+?>
